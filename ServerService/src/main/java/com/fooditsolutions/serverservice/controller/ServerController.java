84f65598ecb0a4887c3ef65c72a3b1092ca2f418
matt-OWN-IT <matt@own-it.be>
2023-05-02 08:37:48 +0200

    Create ServerController.java

---
+++ ServerService/src/main/java/com/fooditsolutions/serverservice/controller/ServerController.java
@@ -0,0 +1,16 @@
+package com.fooditsolutions.serverservice.controller;
+
+import com.fooditsolutions.util.model.Contract;
+import com.fooditsolutions.util.model.Server;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerController {
+    public static List<Server> createServerInformation(String jsonContracts) throws IOException {
+            List<Server> result = new ArrayList<>();
+
+            return result;
+    }
+}
